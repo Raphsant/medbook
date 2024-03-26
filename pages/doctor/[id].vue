de859dad11e8a353817d58a2547208164aa9b273
Rafael Santana <rsantana@colum.edu>
2024-03-26 16:12:59 -0500

    Refactor doctor appointment page and improve user interface
    This commit refactors the page for scheduling doctor appointments and significantly improves the user interface by enhancing code readability and making several style changes. The codebase has been updated to contain more concise and readable code by improving formatting and removing excessive white spaces. Also, the API endpoint has been changed to local host for testing purposes. Moreover, adjustments to the styling make the appointment selection process clearer and more user-friendly. These improvements enhance the overall user experience on the appointment scheduling page.

--- pages/doctor/[id].vue
+++ pages/doctor/[id].vue
@@ -1,5 +1,4 @@
 <template>
-
   <div class="w-screen flex justify-center items-center">
     <UCard class="w-1/2">
       <template #header>
@@ -14,44 +13,87 @@
         <div>Fechas Disponibles</div>
       </div>
       <UContainer
-          class=" h-fit lg:h-[22rem] flex flex-col gap-4 py-2 lg:grid-cols-3 justify-center items-center place-items-center lg:grid">
-        <UButton @click="selectedDate = {
+        class="h-fit lg:h-[22rem] flex flex-col gap-4 py-2 lg:grid-cols-3 justify-center items-center place-items-center lg:grid"
+      >
+        <UButton
+          @click="
+            selectedDate = {
               selectedDateStr: date,
               startTime: getTimeSlotsForDate(date, doctor.schedule).startTime,
-              endTime: getTimeSlotsForDate(date, doctor.schedule).endTime
-            }" :key="date" :variant="selectedDate.selectedDateStr === date ? 'outline' : 'solid'"
-                 class="w-[10rem] flex flex-col justify-center items-center"
-                 v-for="date in upcomingAvailableDates">
+              endTime: getTimeSlotsForDate(date, doctor.schedule).endTime,
+            }
+          "
+          :key="date"
+          :variant="selectedDate.selectedDateStr === date ? 'outline' : 'solid'"
+          class="w-[10rem] flex flex-col justify-center items-center"
+          v-for="date in upcomingAvailableDates"
+        >
           <div>{{ date }}</div>
-          <div>{{ convertTo12Hours(getTimeSlotsForDate(date, doctor.schedule).startTime) }}</div>
-          <div>{{ convertTo12Hours(getTimeSlotsForDate(date, doctor.schedule).endTime) }}</div>
+          <div>
+            {{
+              convertTo12Hours(
+                getTimeSlotsForDate(date, doctor.schedule).startTime,
+              )
+            }}
+          </div>
+          <div>
+            {{
+              convertTo12Hours(
+                getTimeSlotsForDate(date, doctor.schedule).endTime,
+              )
+            }}
+          </div>
         </UButton>
       </UContainer>
-      <UDivider label="Hora Disponible" v-if="selectedDate"/>
-      <UContainer v-if="selectedDate"
-                  class="h-fit gap-4 grid grid-cols-2 py-2  lg:h-[16rem] lg:grid-cols-4 justify-center items-center place-items-center lg:grid">
-
-        <UButton @click="selectedTime = slot" :variant="selectedTime === slot ? 'outline' : 'solid'"
-                 class="text-center flex justify-center items-center w-[5.8rem]"
-                 v-for="slot in generateDateTimeSlots(selectedDate?.selectedDateStr, selectedDate.startTime, selectedDate.endTime, bookedApts)">
-          <div> {{ convertTo12Hours(slot) }}</div>
+      <UDivider label="Hora Disponible" v-if="selectedDate" />
+      <UContainer
+        v-if="selectedDate"
+        class="h-fit gap-4 grid grid-cols-2 py-2 lg:h-[16rem] lg:grid-cols-4 justify-center items-center place-items-center lg:grid"
+      >
+        <UButton
+          @click="selectedTime = slot"
+          :variant="selectedTime === slot ? 'outline' : 'solid'"
+          class="text-center flex justify-center items-center w-[5.8rem]"
+          v-for="slot in generateDateTimeSlots(
+            selectedDate?.selectedDateStr,
+            selectedDate.startTime,
+            selectedDate.endTime,
+            bookedApts,
+          )"
+        >
+          <div>{{ convertTo12Hours(slot) }}</div>
         </UButton>
       </UContainer>
       <template v-if="selectedDate && selectedTime" #footer>
-        <UButton v-if="!isLoading" @click="handleClick" block color="gray">Agendar Cita</UButton>
-        <UButton v-if="isLoading" loading @click="handleClick" block color="gray">Por favor espere...</UButton>
+        <UButton v-if="!isLoading" @click="handleClick" block color="gray"
+          >Agendar Cita</UButton
+        >
+        <UButton
+          v-if="isLoading"
+          loading
+          @click="handleClick"
+          block
+          color="gray"
+          >Por favor espere...</UButton
+        >
       </template>
     </UCard>
 
     <UModal v-model="showConfirmation">
-      <UCard :ui="{ ring: '', divide: 'divide-y divide-gray-100 dark:divide-gray-800' }">
-        <template #header>
-          Cita Confirmada
-        </template>
-        <div class="text-center">{{ formatDateTime(confirmation.dateTime) }}</div>
-        <div class="text-center">Dr.{{ doctor.firstName }} {{ doctor.lastName }}</div>
+      <UCard
+        :ui="{
+          ring: '',
+          divide: 'divide-y divide-gray-100 dark:divide-gray-800',
+        }"
+      >
+        <template #header> Cita Confirmada </template>
+        <div class="text-center">
+          {{ formatDateTime(confirmation.dateTime) }}
+        </div>
+        <div class="text-center">
+          Dr.{{ doctor.firstName }} {{ doctor.lastName }}
+        </div>
         <div class="text-center text-gray-400">{{ doctor.specialty }}</div>
-
 
         <template #footer>
           <NuxtLink to="/myapts">
@@ -60,13 +102,11 @@
         </template>
       </UCard>
     </UModal>
-
   </div>
-
 </template>
 
 <script setup>
-import {useAuthStore} from "~/store/auth";
+import { useAuthStore } from "~/store/auth";
 
 const route = useRoute();
 const authStore = useAuthStore();
@@ -74,31 +114,28 @@ const user = JSON.parse(JSON.stringify(authStore.getUser));
 const selectedDate = ref("");
 const selectedTime = ref("");
 const isLoading = ref(false);
-const showConfirmation = ref(false)
-const confirmation = ref("")
-
+const showConfirmation = ref(false);
+const confirmation = ref("");
 
 async function getDoctor() {
-  const url = `https://postgresapp-e83cc2ceb04b.herokuapp.com/api/getDoctor?id=${route.params.id}`
+  const url = `http://localhost:8080/api/getDoctor?id=${route.params.id}`;
   try {
     const res = await $fetch(url, {
       method: "GET",
       headers: {
         "x-access-token": user.token,
       },
-
     });
     if (res) {
-      console.log(res)
+      console.log(res);
       return JSON.parse(JSON.stringify(res));
     }
-
   } catch (e) {
     console.error(e);
   }
 }
 
-const doctor = await getDoctor()
+const doctor = await getDoctor();
 
 function generateNext14DaysInSpanish(schedule) {
   let dates = [];
@@ -108,38 +145,41 @@ function generateNext14DaysInSpanish(schedule) {
     let futureDate = new Date(today);
     futureDate.setDate(futureDate.getDate() + i);
 
-    let dayOfWeek = futureDate.toLocaleDateString('en-US', {weekday: 'long'});
+    let dayOfWeek = futureDate.toLocaleDateString("en-US", { weekday: "long" });
 
     // Check if the day is active in the doctor's schedule
     if (schedule[dayOfWeek] && schedule[dayOfWeek].active) {
-      dates.push(futureDate.toLocaleDateString('es-ES', {weekday: 'long'}) + " " + futureDate.toLocaleDateString('es-ES', {hour12: true}));
+      dates.push(
+        futureDate.toLocaleDateString("es-ES", { weekday: "long" }) +
+          " " +
+          futureDate.toLocaleDateString("es-ES", { hour12: true }),
+      );
     }
   }
 
   return dates;
 }
 
-
 // Using the function
 let upcomingAvailableDates = generateNext14DaysInSpanish(doctor.schedule);
-upcomingAvailableDates.forEach(date => {
+upcomingAvailableDates.forEach((date) => {
   console.log(date);
 });
 
 function getTimeSlotsForDate(dateString, schedule) {
   // Spanish days of the week mapped to English
   const daysMap = {
-    'lunes': 'Monday',
-    'martes': 'Tuesday',
-    'miércoles': 'Wednesday',
-    'jueves': 'Thursday',
-    'viernes': 'Friday',
-    'sábado': 'Saturday',
-    'domingo': 'Sunday'
+    lunes: "Monday",
+    martes: "Tuesday",
+    miércoles: "Wednesday",
+    jueves: "Thursday",
+    viernes: "Friday",
+    sábado: "Saturday",
+    domingo: "Sunday",
   };
 
   // Extract the day of the week from the string
-  let dayOfWeekSpanish = dateString.split(' ')[0].toLowerCase();
+  let dayOfWeekSpanish = dateString.split(" ")[0].toLowerCase();
 
   // Translate to English
   let dayOfWeekEnglish = daysMap[dayOfWeekSpanish];
@@ -156,10 +196,10 @@ function generateDateTimeSlots(dateString, startTime, endTime, appointments) {
   let slots = [];
 
   // Extract the date part
-  let datePart = dateString.split(' ').slice(1).join(' ');
+  let datePart = dateString.split(" ").slice(1).join(" ");
 
   // Convert date format from "dd/mm/yyyy" to "mm/dd/yyyy"
-  let [day, month, year] = datePart.split('/');
+  let [day, month, year] = datePart.split("/");
   let formattedDate = `${month}/${day}/${year}`;
 
   // Parse the start and end times with the date
@@ -167,19 +207,24 @@ function generateDateTimeSlots(dateString, startTime, endTime, appointments) {
   let endDateTime = new Date(`${formattedDate} ${endTime}`);
 
   if (isNaN(startDateTime) || isNaN(endDateTime)) {
-    return 'Invalid date';
+    return "Invalid date";
   }
 
   while (startDateTime < endDateTime) {
-    let timeString = startDateTime.getHours().toString().padStart(2, '0') + ':' +
-        startDateTime.getMinutes().toString().padStart(2, '0');
+    let timeString =
+      startDateTime.getHours().toString().padStart(2, "0") +
+      ":" +
+      startDateTime.getMinutes().toString().padStart(2, "0");
 
-    let slotDateTime = new Date(`${formattedDate} ${timeString}`,);
+    let slotDateTime = new Date(`${formattedDate} ${timeString}`);
 
     // Check if the time slot is not in any of the appointments
-    let isSlotAvailable = !appointments.some(appointment => {
+    let isSlotAvailable = !appointments.some((appointment) => {
       let appointmentDate = new Date(appointment.dateTime);
-      appointmentDate.toLocaleString("es-ES", {timeZone: 'America/Chicago', hour12: true})
+      appointmentDate.toLocaleString("es-ES", {
+        timeZone: "America/Chicago",
+        hour12: true,
+      });
       return slotDateTime.getTime() === appointmentDate.getTime();
     });
 
@@ -194,65 +239,65 @@ function generateDateTimeSlots(dateString, startTime, endTime, appointments) {
 
 async function handleClick() {
   isLoading.value = true;
-  await new Promise((r) => setTimeout(r, 1000))
-  let datePart = selectedDate.value.selectedDateStr.split(' ')[1];
+  await new Promise((r) => setTimeout(r, 1000));
+  let datePart = selectedDate.value.selectedDateStr.split(" ")[1];
 
   // Split the datePart into day, month, and year
-  let [day, month, year] = datePart.split('/');
+  let [day, month, year] = datePart.split("/");
 
   // Format the date in "YYYY-MM-DD" format
-  let formattedDate = `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`;
+  let formattedDate = `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`;
 
   // Combine date and time
   let dateTime = `${formattedDate}T${selectedTime.value}:00`; // Adding ":00" for seconds
-  console.log(dateTime)
+  console.log(dateTime);
 
   try {
-    let targetDate = new Date(dateTime)
+    let targetDate = new Date(dateTime);
     let targetDateUTC = targetDate.toUTCString();
-    const res = await $fetch("https://postgresapp-e83cc2ceb04b.herokuapp.com/api/apts/create", {
+    const res = await $fetch("http://localhost:8080/api/apts/create", {
       method: "POST",
       headers: {
         "x-access-token": user.token,
       },
-      body:
-          {
-            userId: user.id,
-            doctorId: doctor.id,
-            dateTime: targetDateUTC,
-          }
-
+      body: {
+        userId: user.id,
+        doctorId: doctor.id,
+        dateTime: targetDateUTC,
+      },
     });
     if (res) {
-      console.log(res)
-      confirmation.value = res
+      console.log(res);
+      confirmation.value = res;
       isLoading.value = false;
       showConfirmation.value = true;
     }
   } catch (e) {
-    console.error(e.message)
+    console.error(e.message);
   }
-
 }
 
 async function getApts() {
   try {
     console.log(doctor.id);
-    const res = await $fetch("https://postgresapp-e83cc2ceb04b.herokuapp.com/api/apts/getDoctorApts", {
+    const res = await $fetch("http://localhost:8080/api/apts/getDoctorApts", {
       method: "get",
       headers: {
         "x-access-token": user.token,
       },
       params: {
-        doctorId: doctor.id
-      }
+        doctorId: doctor.id,
+      },
     });
 
     if (res) {
       // Convert the dateTime of each appointment to Chicago time
-      const chicagoTimeZone = 'America/Chicago';
-      res?.forEach(appointment => {
-        let chicagoTime = new Date(appointment.dateTime).toLocaleString('en-US', {timeZone: chicagoTimeZone});
+      const chicagoTimeZone = "America/Chicago";
+      res?.forEach((appointment) => {
+        let chicagoTime = new Date(appointment.dateTime).toLocaleString(
+          "en-US",
+          { timeZone: chicagoTimeZone },
+        );
         appointment.dateTime = chicagoTime;
       });
 
@@ -273,38 +318,35 @@ function formatDateTime(dateTimeString) {
 
   // Options for toLocaleString to get day of the week, date, and time in 12-hour format
   let options = {
-    weekday: 'long', // day of the week
-    day: '2-digit',  // day in two digits
-    month: '2-digit', // month in two digits
-    year: '2-digit',  // two-digit year
-    hour: 'numeric',  // hour
-    minute: 'numeric', // minutes
-    hour12: true  // 12-hour format
+    weekday: "long", // day of the week
+    day: "2-digit", // day in two digits
+    month: "2-digit", // month in two digits
+    year: "2-digit", // two-digit year
+    hour: "numeric", // hour
+    minute: "numeric", // minutes
+    hour12: true, // 12-hour format
   };
 
   // Convert to Spanish locale
-  console.log("Function FormatDateTime")
-  console.log(date.toLocaleString('es-ES', options))
-  return date.toLocaleString('es-ES', options);
+  console.log("Function FormatDateTime");
+  console.log(date.toLocaleString("es-ES", options));
+  return date.toLocaleString("es-ES", options);
 }
 
 function convertTo12Hours(timeString) {
   // Split the time string into hours and minutes
-  let [hours, minutes] = timeString.split(':').map(Number);
+  let [hours, minutes] = timeString.split(":").map(Number);
 
   // Determine AM or PM
-  let ampm = hours >= 12 ? 'PM' : 'AM';
+  let ampm = hours >= 12 ? "PM" : "AM";
 
   // Convert hours to 12-hour format
   hours = hours % 12;
   hours = hours ? hours : 12;
 
   // Return the formatted time string
-  return `${hours}:${minutes.toString().padStart(2, '0')} ${ampm}`;
+  return `${hours}:${minutes.toString().padStart(2, "0")} ${ampm}`;
 }
-
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
