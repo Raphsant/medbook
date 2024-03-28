3e055a5acad18f9d5e912fc40ddb79569fef37d6
Rafael Santana <rsantana@colum.edu>
2024-03-28 13:55:10 -0500

    Enhance SEO and improve code cleanliness in app.vue
    Introduced better SEO meta tags, specifically title, description, and image. Corrected minor syntax issues for better code readability: added a missing semicolon and corrected quotes misalignment. Lastly, made a slight spacing correction on a component for consistency.

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="">
     <NuxtLayout>
-      <NuxtPage/>
+      <NuxtPage />
     </NuxtLayout>
 
     <UNotifications />
@@ -13,12 +13,19 @@ import Login from "./pages/login";
 import { useAuthStore } from "~/store/auth";
 const authStore = useAuthStore();
 const user = JSON.parse(JSON.stringify(authStore.getUser));
-console.log(user)
+console.log(user);
 
-
+useSeoMeta({
+  title: "💊 Centro Clinico Vista Centro",
+  ogTitle: "Centro Clinico Vista Centro",
+  description: "Sistema de Citas, Centro Clinico Vista Centro",
+  ogDescription: "Sistema de Citas, Centro Clinico Vista Centro",
+  ogImage: "/img/vclogolight.png",
+  twitterCard: "summary_large_image",
+});
 </script>
 <style>
-@import url('https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap");
 html {
   background: transparent;
   font-family: "DM Sans", sans-serif;
