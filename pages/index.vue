c0cde82d81be0cf6c88fb33e155f678db6286ed4
Rafael Santana <rsantana@colum.edu>
2024-04-05 13:10:46 -0500

    Improve homepage layout and functionalities
    This update involves modifications to the homepage layout and functionality. Changes include a flourishing carousel on the front page, better text layout with flyout design, and more interactive icons. Additionally, enhanced navigation features have been implemented, along with a better path to the maps link. The update aims to enhance usability and user experience.

--- pages/index.vue
+++ pages/index.vue
@@ -17,61 +17,78 @@ Cuando necesites respuestas sobre tu salud, sabes a donde ir
         ]"
       >
         <template #description>
-          <div class="text-xl">
+          <div class="text-xl mb-2">
             <p>Somos especialistas en atención de prevención y diagnósticos</p>
-            <p class="text-primary">
+            <p class="text-primary text-lg">
               Horarios de atención de lunes a viernes de 07:00 am a 04:00 pm
             </p>
           </div>
         </template>
-        <img
-          src="https://investorplace.com/wp-content/uploads/2019/08/healthcare1600d.jpg"
-          class="w-full rounded-md shadow-xl ring-1 ring-gray-300 dark:ring-gray-700"
-        />
+        <UCarousel
+          ref="carouselRef"
+          v-slot="{ item }"
+          :items="items"
+          :ui="{ item: 'basis-full' }"
+          class="rounded-lg overflow-hidden"
+          indicators
+          arrows
+        >
+          <img :src="item" class="w-full object-cover" draggable="false" />
+        </UCarousel>
       </ULandingHero>
       <ULandingCTA
         class="h-fit p-0"
-        title="¿Por qué escogernos?"
         description="Tenemos 17 años prestando el mejor de los servicios."
       >
-        <div class="flex justify-center items-center gap-4">
-          <div class="h-[10rem] flex flex-col justify-center items-center">
+        <template #title>
+          <div class="text-primary">¿Por qué escogernos?</div>
+        </template>
+        <div class="flex w-full justify-center items-center text-center">
+          <div class="flex w-full flex-col justify-center items-center">
             <UIcon
               class="w-20 h-20 flex-shrink-0"
+              name="i-heroicons-user-group text-primary"
+            />
+            <div class="w-2/3">Más de 3.500 pacientes atendidos</div>
+          </div>
+          <div class="flex w-full flex-col justify-center items-center">
+            <UIcon
+              class="w-20 h-20 flex-shrink-0 text-primary"
               name="i-heroicons-computer-desktop"
             />
-            <div>Equipamiento tecnológico de última generación</div>
+            <div class="w-2/3">
+              Equipamiento tecnológico de última generación
+            </div>
           </div>
-          <div class="flex flex-col justify-center items-center">
+          <div class="flex w-full flex-col justify-center items-center">
             <UIcon
-              class="w-20 h-20 flex-shrink-0"
-              name="i-heroicons-user-group"
-            />
-            Más de 3.500 pacientes atendidos
-          </div>
-          <div class="flex flex-col justify-center items-center">
-            <UIcon
-              class="w-20 h-20 flex-shrink-0"
+              class="w-20 h-20 flex-shrink-0 text-primary"
               name="i-heroicons-hand-thumb-up
 "
             />
-            <div>Trabajamos con aseguradoras y también con particulares</div>
+            <div class="w-2/3">
+              Trabajamos con aseguradoras y también con particulares
+            </div>
           </div>
         </div>
       </ULandingCTA>
       <div class="w-full flex gap-5">
         <ULandingCard
           title="¿Donde encontrarnos?"
-          description="Esquina el chorro a Dr Paul Centro Comercial gallo de oro piso 2 y 3."
+          description="Nos puedes encontrar en la esquina El Chorro a Dr Paul, en el Centro Comercial Gallo de Oro piso 2 y 3."
           icon="i-heroicons-map"
           color="primary"
           orientation="horizontal"
           to="https://www.google.com/maps/place/Centro+Cl%C3%ADnico+Vista+Centro/@10.5040329,-66.9122475,15z/data=!4m6!3m5!1s0x8c2a5ed121d63e91:0xe82aa5cee4f05815!8m2!3d10.5040329!4d-66.9122475!16s%2Fg%2F11c45tmp65?entry=ttu"
         >
-          <img
-            src="https://media.discordapp.net/attachments/684442343168147529/1213197591722856568/Untitled.png?ex=66070e4d&is=65f4994d&hm=32762eabd97ff3b033b6378beec433141a9752b17b3b61b5a03b36d89c5d6886&=&format=webp&quality=lossless"
-            class="w-full h-full object-cover rounded-md"
-          />
+          <div class="flex space-x-2">
+            <img src="/img/image.webp" class="w-4/5 object-cover rounded-md" />
+            <img
+              src="/img/gallo.webp"
+              class="w-4/5 object-cover rounded-md"
+              alt=""
+            />
+          </div>
         </ULandingCard>
       </div>
       <LandingFaqAccordion />
@@ -109,8 +126,24 @@ Cuando necesites respuestas sobre tu salud, sabes a donde ir
 </template>
 
 <script setup>
-definePageMeta({
-  middleware: "auth",
+const items = [
+  "/img/foto1.webp",
+  "/img/foto2.webp",
+  "/img/foto3.webp",
+  "/img/foto4.webp",
+];
+//Carousel logic to make it autoplay
+const carouselRef = ref();
+onMounted(() => {
+  setInterval(() => {
+    if (!carouselRef.value) return;
+
+    if (carouselRef.value.page === carouselRef.value.pages) {
+      return carouselRef.value.select(0);
+    }
+
+    carouselRef.value.next();
+  }, 3000);
 });
 </script>
 <style scoped></style>
